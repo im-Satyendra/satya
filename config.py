188553d98b008062077dd2e3ab358c6e98dd8183
epsatya <caspairuserbot@gmail.com>
2022-04-05 21:13:20 +0530

    ok

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+class Config(object):
+    TG_BOT_TOKEN = "5217702275:AAH0KwikoXAzjEc7qO8V9XiH1-gnTAVnFF4"
+    APP_ID = 2171111
+    API_HASH = "fd7acd07303760c52dcc0ed8b2f73086"
+    AUTH_USERS = set(int(x) for x in ().split())
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    MAX_FILE_SIZE = 50000000
+    TG_MAX_FILE_SIZE = 2097152000
+    FREE_USER_MAX_FILE_SIZE = 50000000
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "")
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
+    MAX_MESSAGE_LENGTH = 4096
+    PROCESS_MAX_TIMEOUT = 3600
+    DEF_WATER_MARK_FILE = ""
+    DB = "mongodb+srv://satya:s4tya@cluster0.rrqhs.mongodb.net/utyfky?retryWrites=true&w=majority"
+    CHAT_ID = -1001724430366
+    LOG_CHANNEL = -1001753919095
---
+++ translation.py
@@ -0,0 +1,21 @@
+class Translation(object):
+    START_TEXT = """Hi {},
+/help for more details!"""
+    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
+    SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
+URL | filename | username | password"""
+    DOWNLOAD_START = "Now Downloading.."
+    UPLOAD_START = "Now Uploading.."
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using me"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@xurluploaderbot"
+    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+    CUSTOM_CAPTION_UL_FILE = "{}"
+    NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
+    HELP_USER = """How to Use Me? JUST SEND LINK OR FILE after you know ehat to do!"""
+    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /genthumbnail to a media album, to generate custom thumbnail"
+    ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
+    CANCEL_STR = "Process Cancelled"
+    ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
+    SLOW_URL_DECED = "TOO SLOW URL"
